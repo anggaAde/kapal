5172276b11b997ed964b02a84f3f1fb68af4a569
anggaAde <isk.dzul@gmail.com>
2015-09-29 05:49:57 +0700

    pesenan pak bowo
    index ke proses belum database

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+<form method="POST" action="proses.php">
+    <input name="submit" type="submit" value="kayu">
+    <input name="submit" type="submit" value="baja">
+    <input name="submit" type="submit" value="fiber">
+</form>
+</body>
+</html>
---
+++ proses.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['submit'])&&$_POST['submit']=='kayu')
+{
+	echo 'kayu';
+}
+if(isset($_POST['submit'])&&$_POST['submit']=='baja')
+{
+	echo 'baja';
+}
+if(isset($_POST['submit'])&&$_POST['submit']=='fiber')
+{
+	echo 'fiber';
+}
+?>
